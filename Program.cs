1727f8f3fa7e97223980b734e992ff7cd998637d
armin <a.shoeibi@alibaba.ir>
2022-04-11 01:06:34 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ChainOfResponsibilityDesignPattern;
+
+SeniorManager seniorManager = new();
+VicePresident vicePresident = new();
+seniorManager.SetSupervisor(vicePresident);
+
+ChiefOperatingOfficer coo = new();
+vicePresident.SetSupervisor(coo);
+
+
+seniorManager.ApproveRequest(new("Mouse and keyboard", 100));
+
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
+
+
+seniorManager.ApproveRequest(new("Laptop", 999));
+
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
+
+
+seniorManager.ApproveRequest(new("Desk", 3000));
